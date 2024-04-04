7bda31c0993589a3454ffb636107ac533606e6a0
ddangme <ssilver121@naver.com>
2024-04-04 17:49:46 +0900

    fix: 스프링 Bean Validation을 위한 코드 삭제

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemController.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemController.java
@@ -21,13 +21,6 @@ import java.util.List;
 public class ValidationItemController {
 
     private final ItemRepository itemRepository;
-    private final ItemValidator itemValidator;
-
-    @InitBinder
-    public void init(WebDataBinder dataBinder) {
-        log.info("init binder {}", dataBinder);
-        dataBinder.addValidators(itemValidator);
-    }
 
     @GetMapping
     public String items(Model model) {
